864796d10d9004aa684cfc7e0a05f10e047ed443
Vlad Coretchi <vlad.coretchi@midainformatica.it>
2021-10-07 18:46:50 +0200

    fix api request

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import { Container } from "react-bootstrap";
 import PBNavbar from "./components/navbar";
 import PhoneCommands from "./components/commands";
 import Phonebook from "./components/phonebook";
-import axios from 'axios';
-
-const GetPhonebook = () => {
-  try {
-    let { data } = axios.get('/api/phonebook');
-    return JSON.parse(data) || [];
-  } catch (error) {
-    return [];
-  }
-}
 
 function App() {
   return (
@@ -19,7 +9,7 @@ function App() {
       <PBNavbar />
       <Container>
         <PhoneCommands />
-        <Phonebook data={GetPhonebook()} />
+        <Phonebook />
       </Container>
     </div>
   );
--- src/components/phonebook.js
+++ src/components/phonebook.js
@@ -1,48 +1,59 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table';
+import { Table, Spinner, Alert } from 'react-bootstrap';
 import PhonebookItem from './phonebookItem';
-import { faBolt, faAt, faUser, faPhone, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt, faAt, faUser, faPhone, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 import { useTable, useSortBy } from 'react-table';
+import axios from 'axios';
+import { useAsync } from 'react-async';
+
+const getPhonebooks = async () => {
+    try {
+        let { data } = await axios.get('/api/phonebook');
+        return JSON.parse(data) || [];
+    } catch (error) {
+        return [];
+    }
+}
 
 function RTable({ columns, data }) {
     const { getTableProps, headerGroups, rows, prepareRow } = useTable({
         columns,
         data
     },
-    useSortBy)
+        useSortBy)
 
     return (
         <Table striped border {...getTableProps()}>
             <thead>
                 {headerGroups.map(headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                        {column.render('Header')}
-                    </th>
-                    ))}
-                </tr>
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>
                 ))}
             </thead>
             <tbody>
                 {rows.map((row, i) => {
-                prepareRow(row);
-                return (
-                    <PhonebookItem
-                        label={row.values.label}
-                        email={row.values.email}
-                        number={row.values.internalPhoneNumber}
-                        mobile={row.values.mobilePhoneNumber}
-                        speed={row.values.speedEnabled} />
-                )
+                    prepareRow(row);
+                    return (
+                        <PhonebookItem
+                            label={row.values.label}
+                            email={row.values.email}
+                            number={row.values.internalPhoneNumber}
+                            mobile={row.values.mobilePhoneNumber}
+                            speed={row.values.speedEnabled} />
+                    )
                 })}
             </tbody>
         </Table>
     );
 }
 
-function Phonebook({ data }) {
+function Phonebook() {
     const columns = [
         {
             Header: () => <span><FontAwesomeIcon icon={faBolt} /></span>,
@@ -66,9 +77,29 @@ function Phonebook({ data }) {
             accessor: 'mobilePhoneNumber'
         }
     ];
+    const dataRequest = useAsync({ promiseFn: getPhonebooks });
 
     return (
-        <RTable columns={columns} data={data} />
+        <div>
+            {dataRequest.isPending &&
+                <div class="text-center">
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+            }
+
+            {dataRequest.error &&
+                <Alert variant="success">
+                    <Alert.Heading>Error</Alert.Heading>
+                    <p>{dataRequest.error}</p>
+                </Alert>
+            }
+            
+            {dataRequest.data &&
+                <RTable columns={columns} data={dataRequest.data} />
+            }
+        </div>
     );
 }
 
